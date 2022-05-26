fa5421fbe09cee05874669fcb45a9bb3979538ff
Robbie Blaine <robbie@zenaptix.com>
2022-05-26 11:14:29 +0200

    WASD movement

--- src/game.rs
+++ src/game.rs
@@ -48,9 +48,13 @@ impl Game {
 
 		let dir = match key {
 			Key::Up => Some(Direction::Up),
+			Key::W => Some(Direction::Up),
 			Key::Down => Some(Direction::Down),
+			Key::S => Some(Direction::Down),
 			Key::Left => Some(Direction::Left),
+			Key::A => Some(Direction::Left),
 			Key::Right => Some(Direction::Right),
+			Key::D => Some(Direction::Right),
 			_ => None,
 		};
 
